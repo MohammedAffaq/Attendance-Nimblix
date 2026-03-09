ba9f148287754d2115e375e7ddc46fc395e08786
Mohammed Affaq <mohammedaffaq1431@gmail.com>
2026-03-09 10:29:38 +0530

    Time issue solved

--- src/main/java/com/nimblix/attendance/AttendanceApplication.java
+++ src/main/java/com/nimblix/attendance/AttendanceApplication.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class AttendanceApplication {
+
+    @PostConstruct
+    public void init() {
+        // Enforce IST time zone globally across the application
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AttendanceApplication.class, args);
     }
