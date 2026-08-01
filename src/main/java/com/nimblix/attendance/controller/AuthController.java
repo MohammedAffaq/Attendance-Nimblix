66fe5a6ff058552944ee7afbfa388018b4fcb677
Mohammed Affaq <mohammedaffaq1431@gmail.com>
2026-08-01 23:31:33 +0530

    Ready for Render deployment

--- src/main/java/com/nimblix/attendance/controller/AuthController.java
+++ src/main/java/com/nimblix/attendance/controller/AuthController.java
@@ -6,11 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.nimblix.attendance.dto.request.LoginRequest;
@@ -26,6 +22,7 @@ import com.nimblix.attendance.service.FileStorageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+//@CrossOrigin(origins = "https://nimblix.netlify.app")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
